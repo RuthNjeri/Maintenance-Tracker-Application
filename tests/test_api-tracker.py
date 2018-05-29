2bc16069e82e890cf10c084b78e1b1eb56e555db
Ruth Waiganjo <ruthwaiganjo@gmail.com>
2018-05-30 00:39:14 +0300

    [feature #157945943] add all the api tests

--- tests/test_api-tracker.py
+++ tests/test_api-tracker.py
@@ -1,10 +1,10 @@
 #test_api-tracker.py
-
-import unittest
-import os
 from app import app
+import unittest
+import json
 
-#the testcase of the api
+
+#The class containing the testcases of the api
 class MaintenanceTrackerApiTest(unittest.TestCase):
 
     def setUp(self):
@@ -18,10 +18,107 @@ class MaintenanceTrackerApiTest(unittest.TestCase):
                         
                         }
 
-    def test_post_request(self):
-        resource = self.app.post('/requests/', data=self.request)
+    def test_create_request(self):
+        """
+        Test that a user can create a request
+        """
+        resource = self.app.post('/users/requests/', data=self.request)
         self.assertEqual(resource.status_code,201)
         self.assertIn(self.request,str(resource.data))
+
+    def test_read_request(self):
+        """
+        Test that a user can read all their requests or 
+        get back an empty dictionary if they do not have requests
+        """
+        resource = self.app.post('/users/requests/',data=self.request)
+        self.assertEqual(resource.status_code,201)
+        resource = self.app.get('/users/requests/')
+        self.assertEqual(resource.status_code,200)
+        self.assertIn(self.request,dict(resource.data))
+
+    def test_read_empty_request(self):
+        """
+        Return an empty dictionary if requests are empty
+        """
+        resource = self.app.post('/users/requests/',data={})
+        self.assertEqual(resource.status_code,201)
+        self.assertIn(self.request,dict(resource.data))
+        resource = self.app.get('/users/requests/')
+        self.assertEqual(resource.status_code,200)
+        self.assertIn(self.request,dict(resource.data))
+ 
+
+    def test_read_request_with_id(self):
+        """
+        Test that a user request is returned
+        when the request ID is specified
+        """    
+        resource = self.app.post('/users/requests/',data=self.request)
+        self.assertEqual(resource.status_code,201)
+        json_result = json.loads(resource.data.decode())
+        resource = self.app.get('/users/requests/{}'.format(json_result['id']))
+        self.assertEqual(resource.status_code,200)
+        self.assertIn(self.request,dict(resource.data))
+
+    def test_no_request_with_id(self):
+        """
+        Test that a user request is not returned
+        when the request ID is specified
+        """ 
+        resource = self.app.get('/users/requests/1')
+        self.assertEqual(resource.status_code,404)
+
+
+    def test_update_requests(self):
+        """
+        Test that a request can be modified
+        """
+        resource = self.app.post('/users/requests/',data=self.request)
+        self.assertEqual(resource.status_code,201)
+        json_result = json.loads(resource.data.decode())
+        resource = self.app.put(
+                                '/users/requests/{}'.format(json_result['id']),
+                                data = {
+                                        'id': 1,
+                                        'title':'Computer Shuts down randomly',
+                                        'description':'The computer beeped three times then shuts down',
+                                        'type':'maintenance' 
+
+                                        }
+                                )
+        self.assertEqual(resource.status_code,200)
+        resource =self.app.get(
+                                '/users/requests/{}'.format(json_result['id'])
+                                )
+        self.assertNotEqual(self.request['title'],resource['title'])
+
+    def test_update_on_request_not_existing(self): 
+        resource = self.app.put(
+                                '/users/requests/5',
+                                data = {
+                                        'id': 5,
+                                        'title':'Computer Shuts down randomly',
+                                        'description':'The computer beeped three times then shuts down',
+                                        'type':'maintenance' 
+
+                                        }
+                                )  
+        self.assertEqual(resource.status_code,404)                         
+
+    def test_delete_request(self):
+        resource = self.app.post('/users/requests/',data=self.request)
+        self.assertEqual(resource.status_code,201)
+        json_result = json.loads(resource.data.decode())
+        resource = self.app.delete('/users/requests/{}'.format(json_result['id']))
+        resource = self.app.get('/users/requests/{}'.format(json_result['id']))
+        self.assertEqual(resource.status_code,404)
+
+    def test_delete_request_not_existing(self):
+        resource = self.app.delete('/users/requests/5')  
+        self.assertEqual(resource.status_code,404)
+        
+
 
 
 if __name__ == "__main__":
