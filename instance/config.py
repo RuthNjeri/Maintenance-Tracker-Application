fe49c432367d0727ab39cf19b2cb971fcbf240cc
Ruth Waiganjo <ruthwaiganjo@gmail.com>
2018-05-30 12:08:54 +0300

    [feature #157945943)]add the tests to signup, signin and read request with none-existing id

---
+++ instance/config.py
@@ -0,0 +1,38 @@
+#config file
+
+import os
+
+class Config(object):
+    """Base config class"""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRETE = os.getenv('SECRET')
+
+
+class DevConfig(Config):
+    """Configuration for development"""
+    DEBUG = True
+
+class TestConfig(Config):
+    """Test config"""
+    TESTING = True
+    DEBUG = True
+
+class StagingConfig(Config):
+    """Staging configuration"""
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Staging for production"""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+            'development':DevConfig,
+            'testing':TestConfig,
+            'staging':StagingConfig,
+            'production':ProductionConfig,
+            }            
+
+
+
